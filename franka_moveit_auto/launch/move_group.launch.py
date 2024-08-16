c3c2ddde421f2980b2f398dd9d1f50e0f05ea802
Matthew Elwin <elwin@northwestern.edu>
2024-08-16 17:32:43 -0500

    Removed old franka configs

--- franka_moveit_auto/launch/move_group.launch.py
+++
@@ -1,8 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("fer", package_name="franka_moveit_auto").to_moveit_configs()
-    moveit_config.move_group_capabilities["capabilities"] = ""
-    return generate_move_group_launch(moveit_config)

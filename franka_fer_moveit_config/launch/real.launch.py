4eae0bb6275fa589e51ceb0e0f77f4250f6fef8f
Matthew Elwin <elwin@northwestern.edu>
2024-10-28 10:30:00 -0500

    Added tentative real launchfile for real robot.
    Needs editing, does not work

---
+++ franka_fer_moveit_config/launch/real.launch.py
@@ -0,0 +1,33 @@
+"""Launchfile for the real robot. Does not start Rviz by default"""
+
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_demo_launch
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # TODO: Need to override the xacro arguments before building the config.  this is done by
+    # Calling .robot_description() and explicitly passing it mappings that make it use the real robot.
+    # Might also take some work to get the controllers loaded properly (although I think changes in the URDF automatically do that once we
+    # generate a URDF for the real robot.)
+    # Need to also pass in robot_ip, possibly to the move_group node
+    # Goal is to run demo mode on real robot,
+    # But also provide a simple setup where we can load a moveit_py node instead of the default move_Group node.
+    moveit_config = MoveItConfigsBuilder("fer", package_name="franka_fer_moveit_config").to_moveit_configs()
+    description = generate_demo_launch(moveit_config)
+
+    # We need to start the gripper separately because it is not implemented as a ROS 2 controller, but rather is a separate node
+    description.add_action(IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_gripper'), 'launch', 'gripper.launch.py']),
+                                    launch_arguments={
+                                        'arm_id' : 'fer',
+                                        'robot_ip': 'None',
+                                        'use_fake_hardware' : 'true'}.items()))
+    # We need a joint_state_publisher to unify the joint states from the gripper and the arm
+    description.add_action(
+        Node(package='joint_state_publisher',
+             executable='joint_state_publisher',
+             parameters=[{'source_list': ['joint_state_broadcaster/joint_states', 'fer_gripper/joint_states'], 'rate': 30}]))
+    return description
+

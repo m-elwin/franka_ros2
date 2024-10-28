db896e75e7fc2427c52a15c1005aada5c222ec3a
Matthew Elwin <elwin@northwestern.edu>
2024-10-28 10:55:22 -0500

    Adding dependencies, debugging real.launch.py

--- franka_fer_moveit_config/launch/real.launch.py
+++ franka_fer_moveit_config/launch/real.launch.py
@@ -2,9 +2,9 @@
 
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -17,12 +17,13 @@ def generate_launch_description():
     # But also provide a simple setup where we can load a moveit_py node instead of the default move_Group node.
     moveit_config = (
         MoveItConfigsBuilder("fer", package_name="franka_fer_moveit_config").
-        robot_description(mapppings={"hand" : "true",
+        robot_description(mappings={"hand" : "true",
                                      "use_fake_hardware" : "false",
                                      "fake_sensor_commands" : "false",
                                      "ros2_control" : "true",
-                                     "robot_ip" : LaunchConfiguration("robot_ip")}).to_moveit_configs()
+                                     "robot_ip" : "panda0.robot"}).to_moveit_configs()
         )
+    print(moveit_config.to_dict()['robot_description'])
     description = generate_demo_launch(moveit_config)
     description.add_action(DeclareLaunchArgument("robot_ip", description="URL or ip address for the robot."))
     # We need to start the gripper separately because it is not implemented as a ROS 2 controller, but rather is a separate node

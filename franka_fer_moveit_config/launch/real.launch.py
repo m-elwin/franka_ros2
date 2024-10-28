53f9c809b4512dfbb499c8632a72e426016284ed
Matthew Elwin <elwin@northwestern.edu>
2024-10-28 14:30:18 -0500

    Working on the real launchfile.
    There is no easy moveit_configs_assistant for the real launchfile
    so writing my own for this purpose.

--- franka_fer_moveit_config/launch/real.launch.py
+++ franka_fer_moveit_config/launch/real.launch.py
@@ -6,36 +6,37 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.conditions import IfCondition
 
 def generate_launch_description():
-    # TODO: Need to override the xacro arguments before building the config.  this is done by
-    # Calling .robot_description() and explicitly passing it mappings that make it use the real robot.
-    # Might also take some work to get the controllers loaded properly (although I think changes in the URDF automatically do that once we
-    # generate a URDF for the real robot.)
-    # Need to also pass in robot_ip, possibly to the move_group node
-    # Goal is to run demo mode on real robot,
-    # But also provide a simple setup where we can load a moveit_py node instead of the default move_Group node.
     moveit_config = (
         MoveItConfigsBuilder("fer", package_name="franka_fer_moveit_config").
         robot_description(mappings={"hand" : "true",
                                      "use_fake_hardware" : "false",
                                      "fake_sensor_commands" : "false",
                                      "ros2_control" : "true",
-                                     "robot_ip" : "panda0.robot"}).to_moveit_configs()
+                                     "robot_ip" : LaunchConfiguration("robot_ip")}).to_moveit_configs()
         )
-    print(moveit_config.to_dict()['robot_description'])
-    description = generate_demo_launch(moveit_config)
-    description.add_action(DeclareLaunchArgument("robot_ip", description="URL or ip address for the robot."))
-    # We need to start the gripper separately because it is not implemented as a ROS 2 controller, but rather is a separate node
-    description.add_action(IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_gripper'), 'launch', 'gripper.launch.py']),
-                                    launch_arguments={
-                                        'arm_id' : 'fer',
-                                        'robot_ip': LaunchConfiguration("robot_ip"),
-                                        'use_fake_hardware' : 'false'}.items()))
-    # We need a joint_state_publisher to unify the joint states from the gripper and the arm
-    description.add_action(
-        Node(package='joint_state_publisher',
-             executable='joint_state_publisher',
-             parameters=[{'source_list': ['joint_state_broadcaster/joint_states', 'fer_gripper/joint_states'], 'rate': 30}]))
+    return LaunchDescription([
+        DeclareLaunchArgument("robot_ip", description="URL or ip address for the robot."),
+        DeclareLaunchArgument("use_rviz", default_value="false", description="Set to True to use Rviz"),
+        # We need to start the gripper separately because it is not implemented as a ROS 2 controller, but rather is a separate node
+        IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_gripper'), 'launch', 'gripper.launch.py']),
+                                 launch_arguments={
+                                     'arm_id' : 'fer',
+                                     'robot_ip': LaunchConfiguration("robot_ip"),
+                                     'use_fake_hardware' : 'false'}.items()),
+        IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),'launch','rsp.launch.py'])),
+        IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),'launch','move_group.launch.py'])),
+        IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),'launch','moveit_rviz.launch.py']),
+                                 condition=IfCondition(LaunchConfiguration('use_rviz'))),
+        Node(package="controller_manager",
+             executable="ros2_control_node",
+             parameters=[PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),'config','fer_real_controllers.yaml'])],
+             remappings=[("/controller_manager/robot_description", "/robot_description")],),
+        IncludeLaunchDescription(PathJoinSubstitution([FindPackageShare('franka_fer_moveit_config'),'launch','spawn_controllers.launch.py']))
+        ])
+
     return description
 
